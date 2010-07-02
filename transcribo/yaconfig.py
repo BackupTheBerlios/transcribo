16469a039e20a4c5af7f5e1c1ceafd11b9a3f577
Leo <devnull@localhost>
2010-07-02 22:01:37 +0200

    merge works again, fixed a bug in the styles when using hyphenation: merging of multiple files may interfere with inheritance of styles as after merging of the new file no dynamic inheritance takes place withinm the first file

--- transcribo/yaconfig.py
+++ transcribo/yaconfig.py
@@ -17,7 +17,6 @@ class Config(dict):
         
 
     def __getattr__(self, name):
-        
         return self[name]
         
     def __setattr__(self, name, value):
@@ -54,23 +53,21 @@ path: a list of path names to search each file name; defaults to [].
         
         
         
-    def merge(self, *args, **kwargs):
+    def merge(self, d):
         '''recursively update the dictionary of config items'''
         
-        def mix_in(d1, d2):
-            for k in d2:
-                if (k in d1 and
-                    isinstance(d1[k], dict) and isinstance(d2[k], dict)):
-                    mix_in(d1[k], d2[k])
-                else:
-                    d1[k] = Config()
-                    d1[k].update(d2[k])
             
         # Construct from the arguments the dict to be mixed in
-        d = Config()
-        for a in args: d.update(a)
-        if kwargs: d.update(kwargs)
-        mix_in(self, d)
+        if not isinstance(d, dict): raise TypeError('dict expected.')
+        if not isinstance(d, Config): d = Config(d)
+        for k in d:
+            if isinstance(d[k], dict) and not isinstance(d[k], Config):
+                d[k] = Config(d[k])
+            if (k in self and isinstance(self[k], Config)
+                and isinstance(d[k], Config)):
+                self[k].merge(d[k])
+            else: self[k] = d[k]
+        
         
     def find_node(self, path, scope):
         path_list = path.split('/')
